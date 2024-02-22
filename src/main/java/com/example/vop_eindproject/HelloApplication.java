81e23b33e574c68ee3d693c9d22275686c4479d2
Aalleepphh <alperen.tok@bazandpoort.be>
2024-02-22 13:42:03 +0100

    added example code

--- src/main/java/com/example/vop_eindproject/HelloApplication.java
+++ src/main/java/com/example/vop_eindproject/HelloApplication.java
@@ -1,20 +1,48 @@
 package com.example.vop_eindproject;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws IOException {
+        Scene scene;
+        VBox root;
+        ListView<String> lijstView;
+        ObservableList<String> observableLijst = FXCollections.observableArrayList();
+        Label keuze;
+
+        root = new VBox(5);
+        scene = new Scene(root, 320, 240);
+        lijstView = new ListView<String>();
+        observableLijst.addAll("Berlijn", "Brussel", "Madrid", "Moskou", "Helsinki");
+        lijstView.setItems(observableLijst);
+        lijstView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        keuze = new Label();
+        lijstView.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                keuze.setText(lijstView.getSelectionModel().getSelectedItem());
+            }
+        });
+
+
+        root.getChildren().addAll(lijstView, keuze);
+        primaryStage.setTitle("Demo lijsten");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
